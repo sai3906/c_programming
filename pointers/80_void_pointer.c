258323552be966d3532e37bd1ca754f5c95cee1f
SAI KIRAN VALLAMKONDA <142433911+sai3906@users.noreply.github.com>
2024-09-05 22:46:55 +0530

    Add files via upload

---
+++ pointers/80_void_pointer.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int a=10;
+    void *p;
+
+    //void pointer can store any data type
+    //we dont need to 
+
+    p=&a;
+    printf("\n%d\n",*(int *)p);
+    //*p dont know how many bytes to acces
+    //so do typecasting/converting to int
+    //so here p knows access 4bytes of int
+    
+    float b=24.5;
+    p=&b;
+    printf("%f\n",*(float *)p);
+
+    char ch='a';
+    p=&ch;
+    printf("%c\n",*(char *)p);
+
+    return 0;
+}
