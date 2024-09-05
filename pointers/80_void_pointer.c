fde08bb2f7ad9f2aece8810a682b5c11a3b3d1ee
SAI KIRAN VALLAMKONDA <142433911+sai3906@users.noreply.github.com>
2024-09-05 22:50:49 +0530

    Add files via upload

--- pointers/80_void_pointer.c
+++ pointers/80_void_pointer.c
@@ -4,12 +4,13 @@ int main(){
     void *p;
 
     //void pointer can store any data type
-    //we dont need to 
+    //we dont need to use many data type pinters
+    //malloc and calloc functions return void poiters
 
     p=&a;
     printf("\n%d\n",*(int *)p);
     //*p dont know how many bytes to acces
-    //so do typecasting/converting to int
+    //so do typecasting/converting to int by (int *)
     //so here p knows access 4bytes of int
     
     float b=24.5;
