47c53652ac59d89f9c1ff51666df33baa6c5395a
SAI KIRAN VALLAMKONDA <142433911+sai3906@users.noreply.github.com>
2024-09-09 22:59:04 +0530

    Add files via upload

---
+++ functions/85_function_declaration.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int sum();//function declaration
+//if we dont write anything in funcation argument,if we provide arguments it wont give any error
+//it assumes default return type is int
+//so it is better to write void in function declaration argument --> it wont take any arguments now-->which is good in one way
+
+
+char letter();
+//its better to declare a function globally
+
+
+void main(){
+    sum(8,9);//calling a function
+
+    // char letter();//you can declare a function in main function before calling it
+
+
+    char ch;
+    ch=letter();
+    printf("ch=%c\n",ch);
+}
+
+
+//defination of function
+int sum(){
+    float a,b,sum;
+    printf("\nenter two numbers\n");
+    scanf("%f %f",&a,&b);
+    sum=a+b;
+    printf("sum=%f\n",sum);
+
+
+
+}
+
+char letter(){
+    char c;
+    printf("enter a character\n");
+    scanf(" %c", &c);
+    return c;
+}
