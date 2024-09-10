203791f859552535456eff3fb4ed291d1b51eb0d
SAI KIRAN VALLAMKONDA <142433911+sai3906@users.noreply.github.com>
2024-09-10 23:22:13 +0530

    Add files via upload

--- functions/86_function_defination.c
+++ functions/86_function_defination.c
@@ -2,11 +2,13 @@
 int sum(int m ,int n);
 //datatype( datatypes are enough);
 //it is not necessary mention m,n
-//it is not necessary to same as i,j
+//here m,n are not necessary same as i,j names
+//here m,n and i,j are called as formal parameters
 
 int main(){
     int a=8,b=6,c;
     c=sum(a,b);
+    //here a,b are called as actual parameters
     printf("c=%d\n",c);
     return 0;
 }
