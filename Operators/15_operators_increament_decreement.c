dbc6f1728c2c4c7a940f24bfed5576a4d788cab7
SAI KIRAN VALLAMKONDA <142433911+sai3906@users.noreply.github.com>
2024-09-12 19:28:19 +0530

    Add files via upload

--- Operators/15_operators_increament_decreement.c
+++ Operators/15_operators_increament_decreement.c
@@ -3,31 +3,31 @@ int main(){
     int y,a=12;
     y=++a;
     //after = ++ is there.means it should increase first and assign later
-    //i.e a=a+1
-    //y=a
-    printf("\na=%d\n",a);
-    printf("y=%d\n",y);
+    //i.e a=a+1 means a is modified to a=13
+    //y=a means y=13
+    printf("\na=%d\n",a);//-->a=13
+    printf("y=%d\n",y);//-->y=13
 
     y=a++;
     //after = a is there.means it should assign first and increament later
-    //i.e y=a
-    //a=a+1
-    printf("\na=%d\n",a);
-    printf("y=%d\n",y);
+    //i.e y=a means y=13
+    //then a is modified as a=a+1 means a=14 
+    printf("\na=%d\n",a);//-->a=14
+    printf("y=%d\n",y);//-->y=13
 
     y=a--;
     //after = a is there.means it should assign first and decreament later
-    //i.e y=a
-    //a=a-1
-    printf("\na=%d\n",a);
-    printf("y=%d\n",y);
+    //i.e y=a means y=14
+    //a is modified as a=a-1 means a=13
+    printf("\na=%d\n",a);//-->a=13
+    printf("y=%d\n",y);//-->y=14
 
     y=--a;
     //after = a is there.means it should decreament first and assign later
-    //i.e y=a
-    //a=a-1
-    printf("\na=%d\n",a);
-    printf("y=%d\n",y);
+    //a=a-1 means a is modified as a=12
+    //then y=a means y=12
+    printf("\na=%d\n",a);//-->a=12
+    printf("y=%d\n",y);//-->y=12
 
     return 0;
 }
