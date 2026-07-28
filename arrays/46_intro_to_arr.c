a6878bbb5b1fce1d25da33092f78f1945f965d47
sai3906 <sai3906@gmail.com>
2026-07-28 21:53:29 +0530

    jul28,2026

---
+++ arrays/46_intro_to_arr.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main(){
+  /*
+  int b
+  here "b" can only store one number
+  if want to store 100 numbers no need to declare 100 integer variables
+  
+  int b[100];
+  under a single varible name, array can store same datatype multiple values in a continous memory
+  here "b" can store 100 integer values using the same name
+
+  datatype name[size in +ve int]
+  int b[-6]  => not valid
+  int b[3+3] => valid
+  int b[ ]   => not valid
+  */
+  int a=6;
+  int s[a]; //s can store 6 integer values
+  printf("\nsize=%ld bytes\n",sizeof(a));
+  printf("size=%ld bytes\n",sizeof(s));
+  
+  int y[11/2];  //since 11/2 is 5 ,it works
+  printf("size=%ld bytes\n",sizeof(y));
+}
